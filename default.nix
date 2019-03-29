64a12ad7487a13302008237c44c875bb1704966c
Alexei Pastuchov <info@maximka.de>
2019-03-29 11:48:35 +0100

    +autopep8

--- default.nix
+++ default.nix
@@ -1,13 +1,15 @@
 let pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
   buildInputs = [
-    pkgs.python35Packages.python
-    pkgs.python35Packages.tox
-    pkgs.python35Packages.sphinx
-    pkgs.python35Packages.pytest
+    pkgs.python35Packages.autopep8
     pkgs.python35Packages.pip
+    pkgs.python35Packages.pycodestyle
     pkgs.python35Packages.pylint
+    pkgs.python35Packages.pytest
+    pkgs.python35Packages.python
     pkgs.python35Packages.setuptools
+    pkgs.python35Packages.sphinx
+    pkgs.python35Packages.tox
   ];
   shellHook = ''
 alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
@@ -19,5 +21,6 @@ pip install pytest-random
 pip install virtualenv
 PATH="$PATH:$(pwd)/_build/pip_packages/bin"
 alias tox="TOX_TESTENV_PASSENV=\"PYTHONPATH\" \tox"
+alias autopep8="autopep8 --in-place --max-line-length=80 --aggressive"
 '';
 }
