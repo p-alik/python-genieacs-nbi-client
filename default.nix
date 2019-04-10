297d3d3c6e3401200c1d91ca561fd30737e33693
Alexei Pastuchov <info@maximka.de>
2019-04-10 15:24:36 +0200

    install only pytest-cov and pytest-random by pip inside of /run/user/1000

--- default.nix
+++ default.nix
@@ -6,21 +6,22 @@ in pkgs.mkShell {
     pkgs.python35Packages.pycodestyle
     pkgs.python35Packages.pylint
     pkgs.python35Packages.pytest
+    pkgs.python35Packages.pytest-virtualenv
+    pkgs.python35Packages.pytestcov
+    pkgs.python35Packages.pytestflakes
     pkgs.python35Packages.python
     pkgs.python35Packages.setuptools
     pkgs.python35Packages.sphinx
     pkgs.python35Packages.tox
   ];
+
   shellHook = ''
-alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
-export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.5/site-packages:$PYTHONPATH"
-pip install pytest-flakes
-pip install pytest-cov
-pip install pytest-pylint
-pip install pytest-random
-pip install virtualenv
-PATH="$PATH:$(pwd)/_build/pip_packages/bin"
+alias pip="PIP_PREFIX='$TEMP/_build/pip_packages' \pip"
+export PYTHONPATH="$TEMP/_build/pip_packages/lib/python3.5/site-packages:$PYTHONPATH"
+PATH="$TEMP/_build/pip_packages/bin:$PATH"
 alias tox="TOX_TESTENV_PASSENV=\"PYTHONPATH\" \tox"
 alias autopep8="autopep8 --in-place --max-line-length=80 --aggressive"
+pip install pytest-pylint
+pip install pytest-random
 '';
 }
