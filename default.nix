9bfd1a31191eac67b11fa0e290ef4d7b9b3ae7f0
Alexei Pastuchov <info@maximka.de>
2019-04-25 14:53:56 +0200

    cleanup nix configuration

--- default.nix
+++ default.nix
@@ -3,16 +3,6 @@ in pkgs.mkShell {
   buildInputs = [
     pkgs.python35Packages.autopep8
     pkgs.python35Packages.pip
-    pkgs.python35Packages.pycodestyle
-    pkgs.python35Packages.pylint
-    pkgs.python35Packages.pytest
-    pkgs.python35Packages.pytest-virtualenv
-    pkgs.python35Packages.pytestcov
-    pkgs.python35Packages.pytestflakes
-    pkgs.python35Packages.python
-    pkgs.python35Packages.setuptools
-    pkgs.python35Packages.sphinx
-    pkgs.python35Packages.tox
   ];
 
   shellHook = ''
@@ -21,7 +11,8 @@ export PYTHONPATH="$TEMP/_build/pip_packages/lib/python3.5/site-packages:$PYTHON
 PATH="$TEMP/_build/pip_packages/bin:$PATH"
 alias tox="TOX_TESTENV_PASSENV=\"PYTHONPATH\" \tox"
 alias autopep8="autopep8 --in-place --max-line-length=80 --aggressive"
-pip install pytest-pylint
-pip install pytest-random
+alias rst2html="rst2html.py"
+pip install sphinx
+pip install tox
 '';
 }
